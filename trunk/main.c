766667a386756aeab5e9a7997623ddc40767ae27
patrik <patrik@5770939b-c81b-0410-8ef7-af58ab02edb7>
2006-08-24 21:38:17 +0000

    Initial import

---
+++ trunk/main.c
@@ -0,0 +1,34 @@
+#include <gtk/gtk.h>
+#include <glade/glade.h>
+
+
+
+// Callback Funktionen
+
+void some_signal_handler_func(GtkWidget *widget, gpointer user_data) {
+  /* do something useful here */
+}
+
+
+void on_button_settings_clicked(GtkWidget *widget, gpointer user_data)
+{
+	g_print("Einstellungen gedrückt!\n");
+}
+
+int main(int argc, char *argv[])
+{
+    GladeXML *xml;
+
+    gtk_init(&argc, &argv);
+
+    /* load the interface */
+    xml = glade_xml_new("tractasono.glade", NULL, NULL);
+
+    /* connect the signals in the interface */
+    glade_xml_signal_autoconnect(xml);
+
+    /* start the event loop */
+    gtk_main();
+
+    return 0;
+}
