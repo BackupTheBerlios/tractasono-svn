4ae7669f29b6fa5930638f4e91f42af85daf9855
patrik <patrik@5770939b-c81b-0410-8ef7-af58ab02edb7>
2006-08-30 16:54:04 +0000

    kommentar

--- trunk/main.c
+++ trunk/main.c
@@ -7,6 +7,7 @@
 // Globale Variablen
 GladeXML *xml;
 GladeXML *xmlSettings;
+GladeXML *xmlKeyboard;
 
 // Allgemeiner Destroy-Event
 void on_main_window_destroy(GtkWidget *widget, gpointer user_data)
@@ -38,12 +39,22 @@ void on_button_music_clicked(GtkWidget *widget, gpointer user_data)
 	g_print("Musik gedrückt!\n");
 }
 
+
+// Event-Handler für den Musik Button
+void on_entry1_focus(GtkWidget *widget, gpointer user_data)
+{
+	// Hier sollte noch etwas Code rein
+	g_print("Eingabefeld hat Fokus bekommen!\n");
+}
+
+
 // Event-Handler für den Einstellungen Button
 void on_button_settings_clicked(GtkWidget *widget, gpointer user_data)
 {
 	g_print("Einstellungen gedrückt!\n");
 
 	if (xmlSettings == NULL) {
+
 		// Einstellungen Window holen
 		xmlSettings = glade_xml_new("tractasono.glade", "window_settings", NULL);
 		if (xmlSettings == NULL) {
@@ -56,14 +67,27 @@ void on_button_settings_clicked(GtkWidget *widget, gpointer user_data)
 			g_print("Fehler: Konnte vbox5 nicht holen!\n");
 		}
 
-		GtkWidget *vbox3 = NULL;
-		vbox3 = glade_xml_get_widget(xml, "vbox3");
-		if (vbox3 == NULL) {
-			g_print("Fehler: Konnte vbox3 nicht holen!\n");
+		GtkWidget *vbox_keyboard = NULL;
+		vbox_keyboard = glade_xml_get_widget(xml, "vbox_keyboard");
+		if (vbox_keyboard == NULL) {
+			g_print("Fehler: Konnte vbox_keyboard nicht holen!\n");
 		}
 
-		gtk_widget_reparent (vbox5, vbox3);
+		gtk_widget_reparent (vbox5, vbox_keyboard);
 		gtk_widget_show (vbox5);
+
+
+		// Keyboard einblenden
+		xmlKeyboard = glade_xml_new("tractasono.glade", "window_keyboard", NULL);
+	
+		GtkWidget *vbox10 = NULL;
+		vbox10 = glade_xml_get_widget(xmlKeyboard, "vbox10");
+		if (vbox10 == NULL) {
+			g_print("Fehler: Konnte vbox10 nicht holen!\n");
+		}
+	
+		gtk_widget_reparent (vbox10, vbox_keyboard);
+		gtk_widget_show (vbox10);
 	} 
 }
 
@@ -93,13 +117,16 @@ int main(int argc, char *argv[])
 	gtk_init(&argc, &argv);
 	glade_init();
 
-	/* Das Interface laden */
+	// Das Interface laden
 	xml = glade_xml_new("tractasono.glade", "main_window", NULL);
 
-	/* Verbinde die Signale automatisch mit dem Interface */
+	// Verbinde die Signale automatisch mit dem Interface
 	glade_xml_signal_autoconnect(xml);
 
-	/* Programmloop starten */
+	
+
+
+	// Programmloop starten
 	gtk_main();
 
 	return 0;
