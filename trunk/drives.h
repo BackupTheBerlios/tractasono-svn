f41265d075fb8c180cf8e41f19e7f09c9473a946
patrik <patrik@5770939b-c81b-0410-8ef7-af58ab02edb7>
2006-09-25 17:16:35 +0000

    drive-connected wird richtig gehandelt

--- trunk/drives.h
+++ trunk/drives.h
@@ -6,6 +6,8 @@
 #define DRIVES_FAIL 1
 
 int init_drives();
+static void print_drive(GnomeVFSDrive *drive);
+static void print_volume(GnomeVFSVolume *volume);
 void drive_connected(GnomeVFSVolumeMonitor *monitor, GnomeVFSDrive *drive, gpointer data);
 void drive_disconnected(GnomeVFSVolumeMonitor *monitor, GnomeVFSDrive *drive, gpointer data);
 int print_error (GnomeVFSResult result, const char *uri_string);
