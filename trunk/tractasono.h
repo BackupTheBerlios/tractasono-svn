746a0a2a3779896120a971ab8b1b366f52f29d4a
patrik <patrik@5770939b-c81b-0410-8ef7-af58ab02edb7>
2006-11-13 17:15:14 +0000

    tractasono.h added

---
+++ trunk/tractasono.h
@@ -0,0 +1,20 @@
+// Globale Includes
+#include <gtk/gtk.h>
+#include <glade/glade.h>
+
+// Globale Variablen
+GladeXML *xml;
+GtkWidget *mainwindow;
+GtkWidget *vbox_placeholder;
+GtkWidget *vbox_keyboard;
+
+GtkWidget *window_music;
+GtkWidget *window_import;
+GtkWidget *window_settings;
+GtkWidget *window_fullscreen;
+GtkWidget *window_disc;
+
+GtkEntry *actual_entry;
+
+
+
