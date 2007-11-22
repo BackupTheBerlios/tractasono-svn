40691b994f4b5fb4ae9d032b0f5acf892480062f
patrik <patrik@5770939b-c81b-0410-8ef7-af58ab02edb7>
2007-11-22 02:08:24 +0000

    spiderman spiderman

--- trunk/spider/main.c
+++ trunk/spider/main.c
@@ -1,26 +1,24 @@
-/* Copyright (C) 2003 Scott Wheeler <wheeler@kde.org>
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- *
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
- *
- * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
- * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
- * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
- * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
- * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
- * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
- * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
- * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
- * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+/*
+ *      main.c
+ *      
+ *      Copyright 2007 Patrik Obrist <padx@gmx.net>
+ *      
+ *      This program is free software; you can redistribute it and/or modify
+ *      it under the terms of the GNU General Public License as published by
+ *      the Free Software Foundation; either version 2 of the License, or
+ *      (at your option) any later version.
+ *      
+ *      This program is distributed in the hope that it will be useful,
+ *      but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *      GNU General Public License for more details.
+ *      
+ *      You should have received a copy of the GNU General Public License
+ *      along with this program; if not, write to the Free Software
+ *      Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ *      MA 02110-1301, USA.
  */
+
 
 // Includes
 #include <stdio.h>
@@ -28,6 +26,7 @@
 #include <sys/stat.h>
 #include <tag_c.h>
 #include <glib.h>
+#include <glib/gstdio.h>
 
 // Defines
 #define RETURN_SUCCESS	0
@@ -36,8 +35,13 @@
 // Prototypen
 void recursive_dir (const gchar *path);
 void print_file_tags (const gchar *path);
+void create_dir (const gchar *path, gboolean parents);
 gchar *get_export_dir (void);
+gchar *get_artist_dir (const gchar *artist);
 void create_artist_dir (const gchar *artist);
+gchar *get_album_dir (const gchar *album, const gchar *artist);
+void create_album_dir (const gchar *album, const gchar *artist);
+gchar *get_song_path (const gchar *album, const gchar *artist, const gchar *title, gint track);
 
 
 // Hauptprogramm
@@ -52,12 +56,15 @@ int main (int argc, char *argv[])
 	}
 
 	// Braucht es das?
-	taglib_set_strings_unicode(FALSE);
+	taglib_set_strings_unicode(TRUE);
 
 	// Pfade bestimmen
 	importdir = g_strdup(argv[1]);
 	g_message ("import dir: %s", importdir);
 	g_message ("export dir: %s", get_export_dir ());
+	
+	// Export Verzeichnis anlegen
+	create_dir (get_export_dir (), TRUE);
 
 	// Starte Scan
 	recursive_dir (importdir);
@@ -100,46 +107,103 @@ void print_file_tags (const gchar *path)
 {
 	TagLib_File *file;
 	TagLib_Tag *tag;
+	gchar *artist;
+	gchar *title;
+	gchar *album;
+	gint track;
 	
+	// Prüfen ob die Datei gültig ist
 	file = taglib_file_new (path);
-	
 	if (file == NULL) {
 		return;
 	}
 	
+	// Prüfen ob wir Tags haben
 	tag = taglib_file_tag (file);
-
-	//g_message ("%s - %s", taglib_tag_artist(tag), taglib_tag_title(tag));
+	if (tag == NULL) {
+		return;
+	}
 	
-	create_artist_dir (taglib_tag_artist(tag));
+	// Die einzelnen Tags holen
+	artist = taglib_tag_artist(tag);
+	title = taglib_tag_title(tag);
+	album = taglib_tag_album (tag);
+	track = taglib_tag_track (tag);
+	
+	// Tags gegebenenfalls überschreiben
+	if (artist == NULL || strcmp(artist, "") == 0) {
+		artist = g_strdup_printf ("Unknown Artist");
+	}
+	if (title == NULL || strcmp(title, "") == 0) {
+		title = g_strdup_printf ("Unknown Title");
+	}
+	if (album == NULL || strcmp(album, "") == 0) {
+		album = g_strdup_printf ("Unknown Album");
+	}
 
-	/*printf ("-- TAG --\n");
-	printf ("title   - \"%s\"\n", taglib_tag_title(tag));
-	printf ("artist  - \"%s\"\n", taglib_tag_artist(tag));
-	printf ("album   - \"%s\"\n", taglib_tag_album(tag));
-	printf ("year    - \"%i\"\n", taglib_tag_year(tag));
-	printf ("comment - \"%s\"\n", taglib_tag_comment(tag));
-	printf ("track   - \"%i\"\n", taglib_tag_track(tag));
-	printf ("genre   - \"%s\"\n", taglib_tag_genre(tag));
-	*/
-
+	//g_message("Song: %s", get_song_path (album, artist, title, track));
+	
+	// Artist & Album Ordner erstellen
+	create_artist_dir (artist);
+	create_album_dir (album, artist);
+	
+	// Speicher wieder freigeben
 	taglib_tag_free_strings ();
 	taglib_file_free (file);
 }
 
+
+// Vezeichnis mit oder ohne Parent-Vereichnissen erstellen
+void create_dir (const gchar *path, gboolean parents)
+{
+	if (!g_file_test(path, G_FILE_TEST_EXISTS)) {
+		gint ret;
+		if (parents) {
+			ret = g_mkdir_with_parents (path, 493);
+		} else {
+			ret = g_mkdir (path, 493);
+		}
+		if (ret == 0) {
+			g_message ("Directory created: %s", path);	
+		} else {
+			g_message ("Cannot create directory: %s", path);
+		}
+	}
+}
+
+// Gibt Default Export Vezeichnis zurück
 gchar *get_export_dir (void)
 {
 	return g_strdup_printf ("%s/tractasono/music/", g_get_home_dir());
 }
 
+// Gibt den Verzeichnisname für einen Artist zurück
+gchar *get_artist_dir (const gchar *artist)
+{
+	return g_strdup_printf ("%s%s", get_export_dir (), artist);
+}
+
+// Erstellt ein Verzeichnis für einen Artist
 void create_artist_dir (const gchar *artist)
 {
-	gchar *artist_dir;
-	
-	artist_dir = g_strdup_printf ("%s%s", get_export_dir (), artist);
-	
-	if (!g_file_test(artist_dir, G_FILE_TEST_EXISTS)) {
-		g_mkdir (artist_dir, 493);
-		g_message ("Artist dir created: %s", artist_dir);
-	}
+	create_dir (get_artist_dir (artist), FALSE);
+}
+
+// Gibt den Verzeichnisname für einen Album zurück
+gchar *get_album_dir (const gchar *album, const gchar *artist)
+{
+	return g_strdup_printf ("%s%s/%s", get_export_dir (), artist, album);
+}
+
+// Erstellt ein Verzeichnis für einen Album
+void create_album_dir (const gchar *album, const gchar *artist)
+{
+	create_artist_dir (artist);
+	create_dir (get_album_dir (album, artist), FALSE);
+}
+
+// Gibt den Dateiname für einen Song zurück
+gchar *get_song_path (const gchar *album, const gchar *artist, const gchar *title, gint track)
+{
+	return g_strdup_printf ("%s/%.2d %s - %s", get_album_dir (album, artist), track, artist, title);
 }
